0988bbbed84b7ad34d1cfa977b15dcb99f6f5055
GoudarziDeveloper <81271344+GoudarziDeveloper@users.noreply.github.com>
2021-04-06 22:56:02 +0430

    YademanShop 2

--- app/src/main/java/com/example/yademanshop/MainActivity.kt
+++ app/src/main/java/com/example/yademanshop/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+
     }
 }
